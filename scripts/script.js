080b34a4bbdda2739ebe7aadf1cddd1d561b0edb
Vasilii <84954523+V-Ivanich@users.noreply.github.com>
2022-06-14 15:21:32 +0300

    good job

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,33 @@
 
 const allBtn = document.querySelectorAll('.key_1')
-let textT = document.querySelector('.test_text')
+let textIn = document.querySelector('.in_text'),
+textOut = document.querySelector('.out_text'),
+sound = new Audio()
+audio.src="ringout.wav"
+
 
 let baseKey = Array.from(allBtn)
 let spN = ''
-let str = textT.innerText.split('')
+let str = textIn.innerText.split('')
+
 
 function key_D(e){
-  let x = String(e.keyCode)
-  spN += str[0]
-  str.shift()
-  textT.innerHTML = `<span class="painting">${spN}</span>` + str.join('')
   baseKey.forEach(elem => {
-    if(elem.id == x){
+    if(elem.id == e.keyCode){
       elem.classList.add('activeKey')
     }
   })
+  if(str[0] == e.key && e.key != 'Shift'){
+    console.log(str[0]+ '=>' + e.key)
+    textOut.innerHTML += str[0]
+    spN += str[0]
+    str.shift()
+    textIn.innerHTML = `<span class="painting">${spN}</span>` + str.join('')
+  }
+  else {
+    audio.autoplay = true;
+    return
+  }
 }
 
 function  up_key(e){
@@ -25,6 +37,7 @@ function  up_key(e){
       elem.classList.remove('activeKey')
     }
   })
+
 }
 addEventListener('keyup', up_key);
 
