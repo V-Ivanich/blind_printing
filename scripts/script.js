c6a3471613d1923681060fcf49eb9de76e3d16c0
Vasilii <84954523+V-Ivanich@users.noreply.github.com>
2022-06-15 15:12:00 +0300

    add base

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,35 @@
+import {basEngl} from "../modules/base.js"
 
 const allBtn = document.querySelectorAll('.key_1')
 let textIn = document.querySelector('.in_text'),
-textOut = document.querySelector('.out_text'),
-sound = new Audio()
-audio.src="ringout.wav"
+textOut = document.querySelector('.out_text')
 
 
-let baseKey = Array.from(allBtn)
-let spN = ''
-let str = textIn.innerText.split('')
+let baseKey = Array.from(allBtn),
+spN = '',
+str =[]
+
+
+
+function play_sound() {
+  document.getElementById('audiotag1').play();
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function printOutRandom(){
+let cikl = 0,
+temp =''
+  for(let i = 0; i< 43; i++){
+    cikl = getRndInteger(0, 95)
+    temp += basEngl[cikl]
+  }
+textIn.innerHTML = temp
+str = textIn.innerText.split('')
+console.log(temp.length)
+}
 
 
 function key_D(e){
@@ -25,7 +46,8 @@ function key_D(e){
     textIn.innerHTML = `<span class="painting">${spN}</span>` + str.join('')
   }
   else {
-    audio.autoplay = true;
+    if(e.key != 'Shift')
+    play_sound()
     return
   }
 }
@@ -36,9 +58,16 @@ function  up_key(e){
     if(elem.id == x){
       elem.classList.remove('activeKey')
     }
+    if(x == 13 && str.lenght == 44){
+      textIn.innerHTML = ''
+      textIn.innerHTML = ''
+      spN = ''
+      printOutRandom()
+    }
   })
-
 }
+
+printOutRandom()
 addEventListener('keyup', up_key);
 
 addEventListener('keydown', key_D);
