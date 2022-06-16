dbe0742ff6fd2493710426ef6985b37d785636e7
Vasilii <84954523+V-Ivanich@users.noreply.github.com>
2022-06-16 13:17:44 +0300

    add new base, remove audio

--- scripts/script.js
+++ scripts/script.js
@@ -5,30 +5,20 @@ let textIn = document.querySelector('.in_text'),
 textOut = document.querySelector('.out_text')
 
 
-let baseKey = Array.from(allBtn),
+let baseKey = Array.from(allBtn), //! кнопки на виртуалке
 spN = '',
 str =[]
 
-
-
-function play_sound() {
-  document.getElementById('audiotag1').play();
-}
 
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 function printOutRandom(){
-let cikl = 0,
-temp =''
-  for(let i = 0; i< 43; i++){
-    cikl = getRndInteger(0, 95)
-    temp += basEngl[cikl]
-  }
-textIn.innerHTML = temp
-str = textIn.innerText.split('')
-console.log(temp.length)
+let temp = getRndInteger(0, 55)
+textIn.innerText = basEngl[temp]
+str = textIn.innerText.split('') //? массив из строки
+
 }
 
 
@@ -39,31 +29,28 @@ function key_D(e){
     }
   })
   if(str[0] == e.key && e.key != 'Shift'){
-    console.log(str[0]+ '=>' + e.key)
     textOut.innerHTML += str[0]
     spN += str[0]
     str.shift()
     textIn.innerHTML = `<span class="painting">${spN}</span>` + str.join('')
   }
-  else {
-    if(e.key != 'Shift')
-    play_sound()
-    return
-  }
+  else return
 }
 
 function  up_key(e){
+  if(e.key == 'Enter' && str.length == 0){
+    textOut.innerText = ''
+    textIn.innerText = ''
+    spN = ''
+    printOutRandom()
+  }
   let x = String(e.keyCode)
+
   baseKey.forEach(elem => {
     if(elem.id == x){
       elem.classList.remove('activeKey')
     }
-    if(x == 13 && str.lenght == 44){
-      textIn.innerHTML = ''
-      textIn.innerHTML = ''
-      spN = ''
-      printOutRandom()
-    }
+    else return
   })
 }
 
